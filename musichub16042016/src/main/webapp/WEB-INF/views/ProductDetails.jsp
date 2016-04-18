3bd671ba089366ec9be3a35c7388f773e7867863
somsubhramkh <somsubhramukherjee@gmail.com>
2016-04-18 17:32:39 +0530

    Added ProductDetails Page
    Functionality not yet implemented

---
+++ musichub16042016/src/main/webapp/WEB-INF/views/ProductDetails.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%@include file="/Header.jsp" %>
+
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src=""
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <ul>
+              <li>Description:</li>
+              <li>Brand:</li>
+              <li>Price:</li>
+            </ul>
+            <a class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<%@include file="/Footer.jsp" %>
--- musichub16042016/target/musichub16042016/app.js
+++ musichub16042016/target/musichub16042016/app.js
@@ -1,6 +1,25 @@
-var app = angular.module('angularTable', []);
+/*var app = angular.module('angularTable', []);
 
 app.controller('listdata',function($scope, $http){
+	
+	$scope.users = []; //declare an empty array
+	$scope.users="${prodData}";
+	
+	
+	
+    (function(response){ 
+        $scope.users = response;  //ajax request to fetch data into $scope.data
+    });
+    
+    $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
+});*/
+
+
+/*app.controller('listdata',function($scope, $http){
+	$scope.prod=${prodData};
 	$scope.users = []; //declare an empty array
     $http.get("products.json").success(function(response){ 
         $scope.users = response;  //ajax request to fetch data into $scope.data
@@ -11,4 +30,37 @@ app.controller('listdata',function($scope, $http){
         $scope.reverse = !$scope.reverse; //if true make it false and vice versa
     }
 });
+*/
 
+
+/*
+var app = angular.module('angularTable', []);
+
+function MyCtrl($scope) {
+    $scope.getName = function (user) {
+        return "Names";
+    };
+
+    $scope.users = JSON.parse("${prodData}");
+    $scope.parJson = function (json) {
+        return JSON.parse("${prodData}");
+    }
+    //console.log(JSON.parse($scope._users[0].User.lastlogin));
+}*/
+
+
+/*var vegetables = [{
+    "name": "Carrot"
+}, {
+    "name": "Potato"
+}, {
+    "name": "Broccoli"
+}];*/
+
+var vegetables = ${pageContext.request.prodData};
+
+angular.module('repeatSample', [])
+    .controller('repeatController', function($scope){
+    	$scope.veggies=vegetables;
+    	
+    });
